c13e7ed98f87de19710a0acc2bcbb7bc87eaf2cd
luisfelipe1006 <luisfelipecmartins794@gmail.com>
2026-08-29 18:46:39 -0300

    Add files via upload

--- Rafaautotech.js
+++ Rafaautotech.js
@@ -1,16 +1,33 @@
- function agendarServico() {
+function agendarServico() {
 
     const telefone = "5511964715306";
-    const mensagem = "Olá! Gostaria de agendar um serviço para o meu veículo na Rafa Auto Tech.";
 
-    const app = `whatsapp://send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
-    const web = `https://web.whatsapp.com/send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
+    const mensagem =
+        "Olá! Gostaria de agendar um serviço para o meu veículo na Rafa Auto Tech.";
 
-    // Tenta abrir o aplicativo
+    const app =
+        `whatsapp://send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
+
+    const web =
+        `https://web.whatsapp.com/send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
+
     window.location.href = app;
 
-    // Se o aplicativo não abrir, vai para o navegador
     setTimeout(function() {
-        window.location.href = app;
-    }, 5000);
+        window.location.href = web;
+    }, 3000);
+}
+
+
+/* GOOGLE MAPS */
+
+function comoChegar() {
+
+    const endereco =
+        "Rua dos Pássaros, 14, Montanhão, São Bernardo do Campo, SP, 09784-060";
+
+    const url =
+        `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(endereco)}`;
+
+    window.open(url, "_blank");
 }
